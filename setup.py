15f26e45cd30db0ee2700486a85cb9739ffcc4ac
YOSHIDA Satoshi <syd@timedia.co.jp>
2019-10-11 13:46:48 +0900

    packaging

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import sys
+from distutils.core import setup
+
+setup(
+  name='primes',
+  version='0.1',
+  author='shidocchi',
+  author_email='shidocchi@gmail.com',
+  license='MIT',
+  url='https://github.com/shidocchi/primes',
+  description='library about prime numbers',
+  long_description='',
+  packages=['primes'],
+  provides=['primes'],
+  keywords='number prime',
+  classifiers=[
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Science/Research',
+    'Natural Language :: English',
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Topic :: Scientific/Engineering :: Mathematics',
+  ],
+)
